cf1d653ce21026937ecdac5185c9140861e4200a
Mona-Alakhras <monaalakhras00@gmail.com>
2026-03-20 05:04:17 +0200

    Assignments

---
+++ Web/JS/Practice1/script.js
@@ -0,0 +1,29 @@
+var favoriteAnimal = "cat";
+console.log(favoriteAnimal);
+
+var a = 15;
+var b = 10;
+var sum = a + b;
+console.log(sum);
+
+var age = 23;
+console.log("I am " + age + " years old.");
+
+var country = "Italy";
+country = "France";
+console.log(country);
+
+const pi = 3.14;
+console.log(pi);
+
+var favoriteColor = "baby blue";
+console.log(favoriteColor);
+
+var x = 5;
+var y = 10;
+console.log(x + y);
+
+var isSunny = true;
+if (isSunny) {
+    console.log("It is sunny today.");
+}
