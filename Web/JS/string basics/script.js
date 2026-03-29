6e262f97bff44e73ef991a72084f5c5475dfc4f2
Mona-Alakhras <monaalakhras00@gmail.com>
2026-03-29 23:21:25 +0300

    Assignments

---
+++ Web/JS/string basics/script.js
@@ -0,0 +1,55 @@
+function removeBlanks(str){
+    var newStr = "";
+    for(var i = 0; i<=str.length-1; i++){
+        if(str[i] !== " "){
+            newStr+= str[i];
+        }
+    }
+    return newStr;
+
+}
+console.log(removeBlanks("Pl ayTha tF nkyM usi c"));
+
+function getDigits(str){
+    var newDigit = "";
+    for(var i =0; i<= str.length-1; i++){
+        if(str[i]>='0' && str[i]<=9){
+            newDigit+= str[i];
+        }
+    }
+    return newDigit;
+}
+console.log(getDigits("abc8c0d1ngd0j0!8"));
+
+function acronym(str){
+    var newOut = "";
+    var strSplit = str.split(" ");
+    for(var i=0; i<=strSplit.length-1;i++){
+        var word = strSplit[i]
+        if(word.length > 0){
+            newOut+= word[0]
+        }
+    }
+    return newOut.toUpperCase();
+}
+
+console.log(acronym("Live from New York, it's Saturday Night!"));
+
+function counNonSpaces(str){
+    var arrayStr = str.split(" ");
+    var newStr = arrayStr.join("")
+    return newStr.length;
+}
+console.log(counNonSpaces("Hello world !"));
+
+function removeShorterStrings(arr, minLength){
+    var newArray = []
+    for(var i = 0; i<arr.length; i++){
+        var nonShortStr = arr[i];
+        if(nonShortStr.length >= minLength){
+            newArray.push(nonShortStr);
+        }
+    }
+    return newArray;
+}
+console.log(removeShorterStrings(['There','is','a','bug','in','the','system'],3))
