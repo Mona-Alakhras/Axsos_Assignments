1dc494e8350bcc042b683cc8c2a21fbd6314ad56
Mona-Alakhras <monaalakhras00@gmail.com>
2026-04-10 18:27:19 +0300

    hello assignment

---
+++ python_stack/python/python_fundamentals/Hello World/hello_world.py
@@ -0,0 +1,13 @@
+print("Hello World")
+name = "Noelle"
+print("Hello", name)
+print("Hello " + name)
+name = 42
+print("Hello", name)
+print("Hello " + str(name))
+fav_food1 = "sushi"
+fav_food2 = "pizza"
+print("I love to eat {} and {}".format(fav_food1,fav_food2))
+print(f"I love to eat {fav_food1} and {fav_food2}")
+#NINJA BONUS
+print("I love to eat %s and %s" %(fav_food1, fav_food2))
