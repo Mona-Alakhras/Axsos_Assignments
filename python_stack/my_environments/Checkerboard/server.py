90a7f9dc3b2c67aaf250550fec483a49dcb4220d
Mona-Alakhras <monaalakhras00@gmail.com>
2026-05-09 10:08:04 +0300

    checkerboard assignment

--- python_stack/my_environments/Checkerboard/server.py
+++ python_stack/my_environments/Checkerboard/server.py
@@ -10,9 +10,9 @@ def index():
 def row_only(x):
     return render_template("index.html", row=x, col=8)
 
-@app.route("/<int:x>/<int:y>")
-def custom_board(x, y):
-    return render_template("index.html", row=x, col=y)
+@app.route("/<int:x>/<int:y>/<color1>/<color2>")
+def custom_board(x, y, color1, color2):
+    return render_template("index.html", row=x, col=y, color1=color1, color2=color2)
 
 if __name__ == "__main__":
     app.run(debug=True)
